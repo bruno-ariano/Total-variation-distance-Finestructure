02e832969e6d65fa9f3b1409d3cddf02f99d6a06
Bruno Ariano <bruno.ariano.87@gmail.com>
2021-06-25 14:02:42 +0100

    Update TVD.R

--- TVD.R
+++ TVD.R
@@ -24,48 +24,54 @@ data_chunks_norm = t(apply(data_chunks, 1, function(x) x/sum(x)))
 
 
 ###### To define groups I cut the tree at arbitrary poit using the treeSlice function
-tree_sliced = treeSlice(ttree, prompt = T)
-#In case the above code give error use the below (slice indicate where you want to cut the tree)
-#treeSlice(ttree,prompt = F,slice = 120000) 
+tree_sliced = treeSlice(rescale(ttree, "delta", 5),prompt = T,orientation="rootwards")
+root_nodes = as.numeric(tree_sliced$tip.label)
 
-                           
-root_nodes = c()
-for(record in tree_sliced){
-  root_nodes = c(root_nodes, findMRCA(ttree, tip = record$tip.label))
-}
+#In case the above code give error use the below (slice indicate where you want to cut the tree)
+#tree_sliced = treeSlice(ttree,prompt = F,slice = 120000) 
+# root_nodes = c()
+# for(record in tree_sliced){
+#   root_nodes = c(root_nodes, findMRCA(ttree, tip = record$tip.label))
+# }
+                          
+#root_nodes = c()
+#for(record in tree_sliced){
+#  root_nodes = c(root_nodes, findMRCA(ttree, tip = record$tip.label))
+#}
 
 #Here I store all pairs of combination between nodes(groups) of the tree
 combn_nodes = combn(root_nodes, 2)
 
 #Function to calculate the copying vectors
-make_copy_vector = function(matrix_chunk, label_group1, label_group2){
+make_copy_vector = function(matrix_chunk, label_group1, label_group2, root_nodes){
   #First I extract the matrix with the labels for each group
   group_matrix = matrix_chunk[label_group1,]
-  #I average by row
+  #First I average by row
   group_matrix_mean = apply(group_matrix,2,mean)
-  #Then I sum accross Donors groups
+  #Then I sum all columns(Donors)
   copy_vector1 = c()
-  for(tree in tree_sliced){
+  for(node in root_nodes){
+      tree = extract.clade(ttree,node)
       copy_vector1 = c(copy_vector1, sum(group_matrix_mean[tree$tip.label]))
   }
-
   group_matrix2 = matrix_chunk[label_group2,]
   group_matrix_mean2 = apply(group_matrix2,2,mean)
   copy_vector2 = c()
-  for(tree in tree_sliced){
+  for(node in root_nodes){
+    tree = extract.clade(ttree,node)
     copy_vector2 = c(copy_vector2, sum(group_matrix_mean2[tree$tip.label]))
   }
   return(list(copy_vector1,copy_vector2))
 }
-
                            
-#All TVD values will be stored in a square matrix with row and col. names given by node label of the root
+#All TVD values will be stored in a square matrix with row and col. names given by node label
 tvd_matrix = matrix(rep(0,length(root_nodes)**2),nrow = length(root_nodes))
 colnames(tvd_matrix) = root_nodes
 rownames(tvd_matrix) = root_nodes
                         
                            
 for(i in seq(ncol(combn_nodes))){
+  
   tree1 = extract.clade(ttree,combn_nodes[1,i])
   tree2 = extract.clade(ttree,combn_nodes[2,i])
   copy_vect = make_copy_vector(data_chunks_norm, tree1$tip.label, tree2$tip.label)
